6071fbe1edbd69df47e04f4808887c54a977d0bc
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 08:41:09 +0700

    Projects

---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section id = 'projects'>
+            <h2> Projects </h2>
+            <ul>
+                <li> Placedotio </li>
+                <li> Entities Extract </li>
+            </ul>
+        </section>
+    )
+}
+
+export default Projects;

85ad74b44a32fae1ef859c7a5f64caca5a99bebb
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 08:41:47 +0700

    Assemble components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from "./components/Header";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className= 'App'>
+            <Header/>
+            <main>
+                <About/>
+                <Projects/>
+                <Contact/>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;

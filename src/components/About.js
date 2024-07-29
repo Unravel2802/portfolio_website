9ba2b0b276fb19de581ae120d6c49770e9602a23
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 08:40:17 +0700

    About me file

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'reeact';
+
+const About = () => {
+    return (
+        <section id = 'about'>
+            <h2> About me </h2>
+            <p> A quick description of myself and my goals </p>
+        </section>
+    )
+}
+
+export default About;

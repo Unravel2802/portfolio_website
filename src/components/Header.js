1455c2d835357d51fde353d682142d5dc9f67cd4
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 10:05:31 +0700

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><a href={'#about'}> About </a></li>
+                    <li><a href={'#experience'}> About </a></li>
                     <li><a href={'#projects'}> Projects </a></li>
                     <li><a href={'#contact'}> Contact </a></li>
                 </ul>

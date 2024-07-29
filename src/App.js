687df55838390746d3a3963029cacdaf912faffe
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 10:03:43 +0700

    Adding experience component to app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import Header from "./components/Header";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
-
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Experience from "./components/Experience";
 function App() {
     return (
-        <div className= 'App'>
-            <Header/>
+        <div className="App">
+            <Header />
             <main>
-                <About/>
-                <Projects/>
-                <Contact/>
+                <About />
+                <Experience />
+                <Projects />
+                <Contact />
             </main>
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
 
 export default App;

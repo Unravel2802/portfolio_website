9cd0ad1fbc579d954eaccf3851e89313d6a4d322
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 08:41:24 +0700

    Footer

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section id = 'contact'>
+            <h2> Contact </h2>
+            <ul>
+                <li> LinkedIn </li>
+                <li> Github </li>
+                <li> Gmail: hoanglyquoc@gmail.com </li>
+                <li> Phone Number: 207-307-1607 </li>
+
+            </ul>
+        </section>
+    )
+}
+
+export default Contact;

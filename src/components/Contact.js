755f1ff975900d6b4eec7964867b4fffaf44e899
Unravel2802 <hoanglyquoc@gmail.com>
2024-07-29 10:03:50 +0700

    Update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,13 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <section id = 'contact'>
-            <h2> Contact </h2>
-            <ul>
-                <li> LinkedIn </li>
-                <li> Github </li>
-                <li> Gmail: hoanglyquoc@gmail.com </li>
-                <li> Phone Number: 207-307-1607 </li>
-
-            </ul>
+        <section id="contact">
+            <h2>Contact</h2>
+            <p>Email: <a href="mailto:hoanglyquoc@gmail.com">hoanglyquoc@gmail.com</a></p>
+            <p>LinkedIn: <a href="https://linkedin.com/in/hoang-ly" target="_blank" rel="noopener noreferrer">linkedin.com/in/hoang-ly</a></p>
+            <p>GitHub: <a href="https://github.com/Unravel2802" target="_blank" rel="noopener noreferrer">github.com/Unravel2802</a></p>
         </section>
-    )
-}
+    );
+};
 
 export default Contact;

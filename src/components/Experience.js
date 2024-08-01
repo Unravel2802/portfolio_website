59595fde36530790152362156b2db538c7c735b6
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-01 09:22:01 +0700

    remove current experience content, update later

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,24 +4,24 @@ const Experience = () => {
     return (
         <section id="experience">
             <h2>Experience</h2>
-            <div>
-                <h3>FPT Information System</h3>
-                <p>Artificial Intelligence Intern (June 2023 – August 2023)</p>
-                <ul>
-                    <li>Developed a machine learning model achieving 95% accuracy in predicting patient wait times.</li>
-                    <li>Streamlined an AI toolkit’s model building and evaluation processes, improving data scientist efficiency by 30%.</li>
-                    <li>Resolved over 30 bugs in an Airflow data processing system, improving reliability by 95%.</li>
-                </ul>
-            </div>
-            <div>
-                <h3>VNPT Technology</h3>
-                <p>Full-Stack Software Engineer Intern (June 2024 – August 2024)</p>
-                <ul>
-                    <li>Designed and implemented a web application using Spring and Angular.js that tracked user expenses.</li>
-                    <li>Built a RESTful API enabling seamless communication between the frontend and backend.</li>
-                    <li>Optimized the application’s performance by 30% through refactoring code and implementing caching.</li>
-                </ul>
-            </div>
+            {/*<div>*/}
+            {/*    <h3>FPT Information System</h3>*/}
+            {/*    <p>Artificial Intelligence Intern (June 2023 – August 2023)</p>*/}
+            {/*    <ul>*/}
+            {/*        <li>Developed a machine learning model achieving 95% accuracy in predicting patient wait times.</li>*/}
+            {/*        <li>Streamlined an AI toolkit’s model building and evaluation processes, improving data scientist efficiency by 30%.</li>*/}
+            {/*        <li>Resolved over 30 bugs in an Airflow data processing system, improving reliability by 95%.</li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <h3>VNPT Technology</h3>*/}
+            {/*    <p>Full-Stack Software Engineer Intern (June 2024 – August 2024)</p>*/}
+            {/*    <ul>*/}
+            {/*        <li>Designed and implemented a web application using Spring and Angular.js that tracked user expenses.</li>*/}
+            {/*        <li>Built a RESTful API enabling seamless communication between the frontend and backend.</li>*/}
+            {/*        <li>Optimized the application’s performance by 30% through refactoring code and implementing caching.</li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
         </section>
     );
 };

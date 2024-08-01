6e1183d7d96ade48aad19ece29a8f524e40bad26
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-01 09:22:24 +0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()}</p>
         </footer>
     );
 };

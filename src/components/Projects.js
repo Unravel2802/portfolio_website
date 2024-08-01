18f72bbe733d4cbda5fa17498cf74df94ef5ac25
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-01 09:22:13 +0700

    remove current projects content, update later

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,26 +4,26 @@ const Projects = () => {
     return (
         <section id="projects">
             <h2>Projects</h2>
-            <div>
-                <h3>Placedotio</h3>
-                <p>React, Firebase, NodeJS (June 2023 – Sept 2023)</p>
-                <ul>
-                    <li>Developed an interactive web application using JavaScript and React.js.</li>
-                    <li>Implemented Firebase authentication to secure user data and provide a seamless login experience.</li>
-                    <li>Utilized Firebase real-time database to ensure synchronized updates to the canvas.</li>
-                    <li>Employed React for server-side operations, ensuring a smooth user experience even during peak usage periods.</li>
-                </ul>
-            </div>
-            <div>
-                <h3>Entities Extract</h3>
-                <p>Chrome Extension, Django, spaCy (May 2024 – July 2024)</p>
-                <ul>
-                    <li>Implemented a text analysis algorithm to identify and return various types of entities.</li>
-                    <li>Designed the extension to assist researchers and data scientists with text data annotation for machine learning tasks.</li>
-                    <li>Integrated features to aid students in breaking down complex texts into recognizable entities.</li>
-                    <li>Proposed future enhancements including user customization options, multi-language support, and integration with platforms like Google Drive and Slack.</li>
-                </ul>
-            </div>
+            {/*<div>*/}
+            {/*    <h3>Placedotio</h3>*/}
+            {/*    <p>React, Firebase, NodeJS (June 2023 – Sept 2023)</p>*/}
+            {/*    <ul>*/}
+            {/*        <li>Developed an interactive web application using JavaScript and React.js.</li>*/}
+            {/*        <li>Implemented Firebase authentication to secure user data and provide a seamless login experience.</li>*/}
+            {/*        <li>Utilized Firebase real-time database to ensure synchronized updates to the canvas.</li>*/}
+            {/*        <li>Employed React for server-side operations, ensuring a smooth user experience even during peak usage periods.</li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <h3>Entities Extract</h3>*/}
+            {/*    <p>Chrome Extension, Django, spaCy (May 2024 – July 2024)</p>*/}
+            {/*    <ul>*/}
+            {/*        <li>Implemented a text analysis algorithm to identify and return various types of entities.</li>*/}
+            {/*        <li>Designed the extension to assist researchers and data scientists with text data annotation for machine learning tasks.</li>*/}
+            {/*        <li>Integrated features to aid students in breaking down complex texts into recognizable entities.</li>*/}
+            {/*        <li>Proposed future enhancements including user customization options, multi-language support, and integration with platforms like Google Drive and Slack.</li>*/}
+            {/*    </ul>*/}
+            {/*</div>*/}
         </section>
     );
 };

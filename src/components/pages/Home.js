17faea1c2da518b849ebf1ada88b2f674adec45c
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-06 15:52:36 +0700

    adding fade in effect for header in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
-const Home  = () => {
+import './Home.css'; // Make sure to create this CSS file
+
+const Home = () => {
     return (
-        <section id = 'home'>
-            <div className={"page"} id={"page"}>
-                <h1>Hi, I'm Hoang. </h1>
-                <h2>Just someone curious in Computer Science</h2>
+        <section id='home'>
+            <div className='page' id='page'>
+                <h1 className='main-title'>Hi, I'm Hoang.</h1>
+                <h2 className='subtitle'>Just someone curious in Computer Science</h2>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default Home;

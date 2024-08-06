ccfcaec317ae2255954113da21f132a78eac4770
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-06 08:59:00 +0700

    simple home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,11 @@
 import React from 'react';
+const Home  = () => {
+    return (
+        <div className={"home-container"}>
+            <h1>Hi, I'm Hoang. </h1>
+            <h2>I'm a student that is curious in various fields of Computer Science</h2>
+        </div>
+    )
+}
 
+export default Home;

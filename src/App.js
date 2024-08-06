bac13eda95b4e9e248c499ac37eb34308adb6d77
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-06 11:26:11 +0700

    add app.css styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Experience from "./components/Experience";
+import './App.css'
 function App() {
     return (
         <div className="App">

7d7e2c4e3a8b9758831f6610b259df92f47edf3d
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-06 10:44:49 +0700

    adding styling to NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import './NavBar.css'
 const NavBar = () => {
     return (
         <header>
             <nav>
                 <ul>
+                    <li><a href={'#home'}> Home </a> </li>
                     <li><a href={'#about'}> About </a></li>
-                    <li><a href={'#experience'}> About </a></li>
                     <li><a href={'#projects'}> Projects </a></li>
+                    <li><a href={'#experience'}> Experience </a></li>
                     <li><a href={'#contact'}> Contact </a></li>
                 </ul>
             </nav>

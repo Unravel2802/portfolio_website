ffcd338bcc2439b389bef6afc912252889a38d6a
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-06 17:00:07 +0700

    adding ripple effect to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css'; // Make sure to create this CSS file
+import $ from 'jquery';
+import 'jquery.ripples';
 
 const Home = () => {
+    useEffect(() => {
+        $('#home').ripples({
+            resolution: 256,
+            dropRadius: 40,
+            perturbance: 0.04
+        });
+
+        // Function to create ripples at mouse position
+        const createRippleAtMouse = (event) => {
+            const $home = $('#home');
+            const x = event.clientX;
+            const y = event.clientY;
+            const dropRadius = 30;
+            const strength = 0.08;
+
+            $home.ripples('drop', x, y, dropRadius, strength);
+        };
+
+        // Add mousemove event listener to create ripples
+        document.getElementById('home').addEventListener('mousemove', createRippleAtMouse);
+
+        // Cleanup on unmount
+        return () => {
+            $('#home').ripples('destroy');
+            document.getElementById('home').removeEventListener('mousemove', createRippleAtMouse);
+        };
+    }, []);
+
     return (
         <section id='home'>
             <div className='page' id='page'>

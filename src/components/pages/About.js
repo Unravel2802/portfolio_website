1851ed1f9a0e57dea0d775777e8c781041c169fa
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-07 17:24:13 +0700

    changing about div class

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,23 @@
 import React from 'react';
-
+import './About.css'
 const About = () => {
     return (
-        <div className={"page"} id={"about"}>
-            <h1>About Me</h1>
-            <p>
-                Hi, I'm Hoang, a computer enthusiast from Hanoi, Vietnam.
-                I'm passionate about applying Machine Learning in fields like medicine and education.
-                My experience at VNPT Technology and FPT Information System has equipped me with skills in
-                full-stack web development and real-world ML applications. Currently, I'm a junior at the
-                University of Massachusetts Amherst.
-            </p>
+        <div className={"about-page"} id={"about"}>
+            <div className={"about-content"}>
+                <h1>About Me</h1>
+                <div className={"about-text"}>
+                    <p>
+                        Hi, I'm Hoang, a computer enthusiast from Hanoi, Vietnam.
+                        I'm passionate about applying Machine Learning in fields like medicine and education.
+                        My experience at VNPT Technology and FPT Information System has equipped me with skills in
+                        full-stack web development and real-world ML applications. Currently, I'm a junior at the
+                        University of Massachusetts Amherst.
+                    </p>
+                </div>
+                <div className={"about-image"}>
+
+                </div>
+            </div>
         </div>
     );
 };

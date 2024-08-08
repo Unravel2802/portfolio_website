4a81960efc00f95c8aecbe173963c3401c9000a1
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-08 08:36:14 +0700

    changing about me content and adding about me image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './About.css'
+import hoang from '../../images/about.jpg';
 const About = () => {
     return (
-        <div className={"about-page"} id={"about"}>
+        <div className={"page about-page"} id={"about"}>
+            <h1>About Me</h1>
             <div className={"about-content"}>
-                <h1>About Me</h1>
                 <div className={"about-text"}>
                     <p>
-                        Hi, I'm Hoang, a computer enthusiast from Hanoi, Vietnam.
-                        I'm passionate about applying Machine Learning in fields like medicine and education.
+                        My name is Ly Quoc Hoang (you can call me Justin), a computer enthusiast from Hanoi,
+                        Vietnam. I'm interested in the application of Machine Learning in other fields like medicine and education.
                         My experience at VNPT Technology and FPT Information System has equipped me with skills in
-                        full-stack web development and real-world ML applications. Currently, I'm a junior at the
-                        University of Massachusetts Amherst.
+                        full-stack web development and real-world ML applications. Right now, I'm a junior studying computer
+                        science and mathematics at the University of Massachusetts Amherst.
                     </p>
                 </div>
                 <div className={"about-image"}>
-
+                    <img src={hoang} alt={"hoang"}/>
                 </div>
             </div>
         </div>

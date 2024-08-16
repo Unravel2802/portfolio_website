c289db47633cd7b825389543aeed831afc697da0
Unravel2802 <hoanglyquoc@gmail.com>
2024-08-16 10:03:18 +0700

    add project styling + project content

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import useRippleEffect from "../effects/RippleEffect";
+import './Project.css';
+import ee from '../../images/ee.webp';
+import wi from '../../images/webicon.png';
 
 const Projects = () => {
     useRippleEffect('#projects')
     return (
         <section id={'projects'}>
-            <div className={"page"} id={"projects"}>
-                <h1>My Projects</h1>
+            <div className={"projects-page"}>
+                <h1 className={'projects-header'}>My Projects</h1>
+                <div className={'projects-content'}>
+                    <div className={'projects-item'}>
+                        <h2>Porfolio Website</h2>
+                        <div className={'projects-image'}>
+                            <img src={wi} alt={'alt web icon'}/>
+                        </div>
+                    </div>
+                    <div className={'projects-item'}>
+                        <h2>Extract Entities</h2>
+                        <div className={'projects-image'}>
+                            <img src={ee} alt={'extract entities chrome extension icon'}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
